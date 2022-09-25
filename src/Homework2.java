0f5ebb91018ff8e9914bf593fe253fb8af125ecf
DimaLifeIsGood <dimonvarauka@yahoo.com>
2022-09-26 00:47:54 +0200

    Initial commit

---
+++ src/Homework2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        Scanner a = new Scanner(System.in);
+        String b= a.nextLine();
+        System.out.println(b);
+    }
+}
